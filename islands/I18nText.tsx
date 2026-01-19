ac915d9add417361777160d32061bcdf81d12df8
Chris <chris-carneiro@users.noreply.github.com>
2026-01-19 18:50:38 +0100

    Simplify I18nText island animation handling

--- islands/I18nText.tsx
+++ islands/I18nText.tsx
@@ -1,32 +1,16 @@
-import { useContext, useEffect, useState } from "preact/hooks";
+import { useContext } from "preact/hooks";
 import { TranslationContext } from "./TranslationContext.tsx";
 import { translate } from "../i18n/labels.ts";
 
-
-// NO animation simple impl
-// export default function I18nText({labelKey}: {labelKey: string}) {
-//   const { lang } = useContext(TranslationContext)!;
-//   return <span>{translate(labelKey, lang)}</span>;
-// }
-
 export default function I18nText({ labelKey }: { labelKey: string }) {
-  const [animating, setAnimating] = useState(false);
   const { lang } = useContext(TranslationContext)!;
 
-  useEffect(() => {
-    setAnimating(true);
-    const timeout = setTimeout(() => setAnimating(false), 300);
-
-    return () => clearTimeout(timeout);
-  }, [lang]);
-
   return (
-    <span key={lang}
-      class={`bg-clip-text inset-0 transition-opacity ${
-        animating ? "animate-fade-in" : ""
-      }`}
+    <span
+      key={lang} // if we need to restart animation based on more than 1 variable => `${lang}:${labelKey}`
+      class="animate-fade-in transition-opacity"
     >
-    {translate(labelKey, lang)}
+      {translate(labelKey, lang)}
     </span>
   );
 }
