6529d50002e4c4d2d336ca06b2468db5f78d0d34
Chris <chris-carneiro@users.noreply.github.com>
2026-01-29 15:50:49 +0100

    Add fancy blinking underscore cursor l33t h4k3z $p0t3d =_=

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,9 @@ export default function Header() {
 function Prompt() {
   return (
     <div>
-      <span className="text-albedo">&gt; _</span>
+      <span className="text-albedo">&gt; 
+        <span class="animate-blink">_</span>
+      </span>
       {/* Commands */}
       {
         /* <span className="ml-4 text-citrinitas">{name}</span>
